38a967f0ede2a1b3255b39fc6e5a586a9dcefe6d
Matthew Hall <maseu21@gmail.com>
2014-04-11 14:33:36 -0500

    SQL Server access added

--- dbcomponent.php
+++ dbcomponent.php
@@ -11,6 +11,7 @@ function check($result, $message) {
 				//print "No Error Occurred";
 				return "No Error Occurred";
 				}
+								
 	
 function dbaccess($server,$database,$userID,$password,$dbtype,$query)
 {
@@ -128,17 +129,129 @@ function dbaccess($server,$database,$userID,$password,$dbtype,$query)
 		}
 		
 	}
-	else
+	elseif (strcmp($dbtype,"mssql")==0)
 	{
+		$query_array = explode(" ",$query);
+		$key = strtolower($query_array[0]);
+		$connectionInfo = array( "Database"=>$database, "UID"=>$userID, "PWD"=>$password);
+		$params = array();
+		$options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
+		
+		if(strcmp($key,"select")==0)
+		{
+
+		$return_data = array();
+		
+		$conn = sqlsrv_connect( $server, $connectionInfo);		
+			check($conn, "connect");			
+			
+		$results = sqlsrv_query($conn, $query,$params,$options);
+				check($results, "query of $query");
+		
+		if (sqlsrv_num_rows($results)>0)
+		{	
+			$return_data = array();
+			//$number_of_fields = mysql_num_fields($results);
+			while ($row = sqlsrv_fetch_array($results))
+			{
+				$data = array();
+				for($i=0; $i<count($row);$i++)
+				{
+					$metadata = sqlsrv_field_metadata($results);
+					$rowname = $metadata[$i]["Name"];
+					if (strcmp($rowname," ")==0||$rowname==null)
+					{ break;}
+						$data[$rowname]=$row[$i];
+					/*for($j=0;$j<count($number_of_fields);$j++)
+					{
+						$rowname = trim(mysql_field_name($results, $j));
+						$data[$rowname]=$row[$j];
+						//array_push($data,$row[$i]);		//mysql_field_name($resul, $i)
+					} */
+				}
+				
+				//$row_json = json_encode($data);
+				array_push($return_data,$data);
+			}
 			$debug = array();
-			$debug["data"] = "Supports only MySql Database Management Systems. try the other connection available";
+			$debug["data"] = "No Error Occured"; 
+			$return_json = array();
+			$return_json["code"] = 200;
+			$return_json["data"] = $return_data;
+			$return_json["debug"] = $debug;
+			
+			print json_encode($return_json);		
+			sqlsrv_free_stmt($results);
+			sqlsrv_close($conn);
+			return json_encode($return_json);		
+		}
+		else
+			{
+			$debug = array();
+			$debug["data"] = "No Error Occured";
+			$return_json = array();
+			$return_json["code"] = 200;
+			$return_json["data"] = " ";
+			$return_json["debug"] = $debug;
+			
+			print json_encode($return_json);		
+			sqlsrv_free_stmt($results);
+			sqlsrv_close($conn);
+			return json_encode($return_json);
+			
+			}	
+		}
+		
+		if(strcmp($key,"insert")==0||strcmp($key,"update")==0||strcmp($key,"delete")==0||strcmp($key,"create")==0||strcmp($key,"alter")==0||strcmp($key,"drop")==0)
+		{
+		
+			$conn = sqlsrv_connect( $server, $connectionInfo);		
+			check($conn, "connect");			
+			
+			$results = sqlsrv_query($conn, $query,$params);
+			check($results, "query of $query");
+			
+			if ($results)
+			{	
+			$debug = array();
+			$debug["data"] = "No Error Occured";
+			$return_json = array();
+			$return_json["code"] = 200;
+			$return_json["data"] = "Successful";
+			$return_json["debug"] = $debug;
+			
+			print json_encode($return_json);		
+			sqlsrv_free_stmt($results);
+			sqlsrv_close($conn);
+			return json_encode($return_json);
+			}
+			else
+			{
+			$debug = array();
+			$debug["data"] = "Error Occured";
 			$return_json = array();
 			$return_json["code"] = 422;
 			$return_json["data"] = "Unsuccesful";
 			$return_json["debug"] = $debug;
 			
 			print json_encode($return_json);		
-			mysql_close($conn);
+			sqlsrv_free_stmt($results);
+			sqlsrv_close($conn);
+			return json_encode($return_json);
+			}
+		}
+		
+	}
+	else
+	{
+			$debug = array();
+			$debug["data"] = "No Support for your Database Management System.";
+			$return_json = array();
+			$return_json["code"] = 422;
+			$return_json["data"] = "Unsuccesful";
+			$return_json["debug"] = $debug;
+			
+			print json_encode($return_json);					
 			return json_encode($return_json);
 	}
 }
